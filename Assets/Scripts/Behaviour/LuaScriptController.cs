31b6f37e52ee925f5e7da12d5fd7fe7a6cc3e239
Jarvis Niu <jarvisniu@gmail.com>
2016-04-10 12:19:45 +0800

    simplify by removing AssetBundle

--- Assets/Scripts/Behaviour/LuaScriptController.cs
+++ Assets/Scripts/Behaviour/LuaScriptController.cs
@@ -45,36 +45,10 @@ public class LuaScriptController : MonoBehaviour {
 
 		CallMethod( AwakeRef );
 	}
-
-// AssetBundle testx.unity3d is built for StandaloneWindows
-#if UNITY_STANDALONE
-	IEnumerator Start() {
-		CallMethod( StartRef );
-
-		// -- sample code for loading binary Asset Bundles --------------------
-		String s = "file:///"+Application.streamingAssetsPath+"/testx.unity3d";
-		WWW www = new WWW(s);
-		yield return www;
-		if(www.assetBundle.mainAsset != null) {
-			TextAsset cc = (TextAsset)www.assetBundle.mainAsset;
-			var status = Lua.L_LoadBytes(cc.bytes, "test");
-			if( status != ThreadStatus.LUA_OK )
-			{
-				throw new Exception( Lua.ToString(-1) );
-			}
-			status = Lua.PCall( 0, 0, 0);
-			if( status != ThreadStatus.LUA_OK )
-			{
-				throw new Exception( Lua.ToString(-1) );
-			}
-			Debug.Log("---- call done ----");
-		}
-	}
-#else
+	
 	void Start() {
 		CallMethod( StartRef );
 	}
-#endif
 
 	void Update() {
 		CallMethod( UpdateRef );
